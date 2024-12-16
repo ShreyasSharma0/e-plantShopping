4623fac7104dedcfa5dc9c091f9ceebdda535a1a
ShreyasSharma0 <shreyas.college.study@gmail.com>
2024-12-16 14:09:51 +0530

    error fixes

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -311,7 +311,11 @@ const handlePlantsClick = (e) => {
                                 <div className="product-title">{plant.name}</div>
                                 <div>{plant.description}</div>
                                 <div>{plant.cost}</div>
-                                <button  className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+                                {addedToCart[plant.name] ? (
+        <button disabled>Added to Cart</button>
+    ) : (
+        <button className='product-button' onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+    )}
                             </div>
                         ))}
                     </div>
