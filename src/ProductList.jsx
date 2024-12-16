58acf31adf423e6cad50e26477be1ff1162a6f95
ShreyasSharma0 <shreyas.college.study@gmail.com>
2024-12-16 14:07:56 +0530

    error fixes

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -284,7 +284,7 @@ const handlePlantsClick = (e) => {
                 <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
             </div>
         </div>
-        {!showPlants? (
+        {!showCart? (
         <div className="product-grid">
             {plantsArray.map((category, index) => (
                         <div key={index} className="product-category">
@@ -296,21 +296,34 @@ const handlePlantsClick = (e) => {
                                         <h3>{plant.name}</h3>
                                         <p>{plant.description}</p>
                                         <p><strong>{plant.cost}</strong></p>
-                                        <button className='product-button' onClick={() => handleAddToCart(plant)}>Add to Cart</button>
                                     </div>
                                 ))}
                             </div>
                         </div>
                     ))}
-        
+            {plantsArray.map((category, index) => (
+                <div key={index}>
+                    <h1><div>{category.category}</div></h1>
+                    <div className="product-list">
+                        {category.plants.map(plant, plantIndex) => (
+                            <div className="product-card" key={plantIndex}>
+                                <img src={plant.image} alt={plant.name} className="product-image" />
+                                <div className="product-title">{plant.name}</div>
+                                <div>{plant.description}</div>
+                                <div>{plant.cost}</div>
+                                <button  className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+                            </div>
+                        )}
+                    </div>
+                </div>
+
+            ))}
         </div>
  ) :  (
     <CartItem onContinueShopping={handleContinueShopping}/>
 )}
     </div>
     );
-
-
 }
 
 export default ProductList;
